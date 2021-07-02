1c72c85ffd597aafba74a08d74bdefff8216f4ca
Pedro <russopedro87@gmail.com>
2021-07-02 20:53:51 -0300

    Adding configuration for CORS

--- src/main/java/com/app/statsservice/service/AuthService.java
+++ src/main/java/com/app/statsservice/service/AuthService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
