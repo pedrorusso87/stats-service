4035c5b4eb03b84c776523b179023e4162c87cc0
Pedro <russopedro87@gmail.com>
2021-11-16 19:55:06 -0300

    Modification of team entity

--- src/main/java/com/app/statsservice/model/entities/Team.java
+++ src/main/java/com/app/statsservice/model/entities/Team.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="teams")
@@ -23,6 +24,14 @@ public class Team {
   // This field will be used to reflect if the team is 'ACTIVE' or 'DELETED'
   @Column
   private String status;
+
+  //The date the record was added to the database
+  @Column
+  private Date dateCreated;
+
+  //The date the team was founded
+  @Column
+  private Date foundationDate;
 
   // user id of the team owner
   @OneToOne (cascade = CascadeType.ALL)
