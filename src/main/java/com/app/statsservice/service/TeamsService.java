02daaec33c6d626ddc34f5976f42d41da6d5d2d0
Pedro <russopedro87@gmail.com>
2021-12-10 23:57:22 -0300

    error handling for add team

--- src/main/java/com/app/statsservice/service/TeamsService.java
+++ src/main/java/com/app/statsservice/service/TeamsService.java
@@ -7,6 +7,7 @@ import com.app.statsservice.model.entities.User;
 import com.app.statsservice.model.response.TeamOwner;
 import com.app.statsservice.model.response.UserTeam;
 import com.app.statsservice.repository.TeamsRepository;
+import com.app.statsservice.repository.UserRepository;
 import com.app.statsservice.service.response.TeamsResponse;
 import com.app.statsservice.service.response.UserTeamsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +26,17 @@ public class TeamsService {
   private final TeamsRepository teamsRepository;
 
   @Autowired
+  private final UserRepository userRepository;
+
+  @Autowired
   private final AuthService authService;
 
   private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-  public TeamsService(TeamsRepository teamsRepository, AuthService authService) {
+  public TeamsService(TeamsRepository teamsRepository, AuthService authService, UserRepository userRepository) {
     this.teamsRepository = teamsRepository;
     this.authService = authService;
+    this.userRepository = userRepository;
   }
   
   public List<TeamsResponse> getTeams() {
@@ -49,9 +54,11 @@ public class TeamsService {
     team.setFoundationDate(request.getFoundationDate());
     team.setStatus("ACTIVE");
     if (request.getTeamOwner() != null) {
-      if(this.teamsRepository.findTeamByNameAndUserId(request.getTeamName(), request.getTeamOwner().getId()).isPresent()) {
+      User user = this.userRepository.findByUsername(request.getTeamOwner().getUsername()).get();
+      if(this.teamsRepository.findTeamByNameAndUserId(request.getTeamName(), user.getId()).isPresent()) {
         throw new TeamNameAlreadyExistsException("El nombre de equipo ya existe");
       } else {
+        request.getTeamOwner().setId(user.getId());
         team.setUser(request.getTeamOwner());
       }
     } else {
